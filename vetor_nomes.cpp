8d3ac288cb3abebf360eb0e2a804dfa358afc082
Daniel Gusmão Coaio <danigcoaio@gmail.com>
2025-11-14 14:30:55 -0300

    Create vetor_nomes.cpp

---
+++ vetor_nomes.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std; 
+#include <string>
+int main() 
+{ 
+   string texto[7] = {"Monique", "Ana", "Lucas", "Aiko", "Coaio", "Miguel","Five"}; 
+   cout << texto[4]; 
+    return 0; 
+}
